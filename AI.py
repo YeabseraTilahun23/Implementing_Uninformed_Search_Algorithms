8800c65961a80d544a3acae595966db90887037e
YeabseraTilahun23 <Yeabtilahun7@gmail.com>
2025-04-03 14:26:43 +0300

    Initial upload for uninformed search algorithms
    # Implementing Breadth-First Search (BFS)
    # Implementing Depth-First Search (DFS)
    # Implementing Uniform-Cost Search (UCS)
    # Implementing Iterative Deepening Depth-First Search (IDDFS) 
     using a graph as an adjacent list (dictionary)

--- AI.py
+++ AI.py
@@ -5,22 +5,20 @@ import heapq
 
 
 graph = {
-'Piasa': ['4 kilo', 'Megenagna', 'Merkato', 'Torhayloch', 'Abinet'],
-'4 kilo': ['Piasa', '5 kilo',],
-'5 kilo': ['4 kilo', 'Bole'],
-'Bole': ['5 kilo', 'Megenagna', 'Kazanchis'],
-'Megenagna': ['Piasa', 'Bole', 'Mexico', 'Koye'],
-'Merkato': ['Piasa', 'Autobistera'],
-'Kazanchis': ['Bole'],
-'Autobistera': ['Merkato'],
-'Mexico': ['Bole', 'Megenagna', '4 kilo'],
-'Torhayloch': ['Mexico'],
-'Abinet': ['Mexico', 'Merkato', 'Piasa'],
-'Koye': ['Megenagna'],
-'Mesalemya': ['Merkato']
-
+    'Piasa': ['4 kilo', 'Megenagna', 'Merkato', 'Torhayloch', 'Abinet'],
+    '4 kilo': ['Piasa', '5 kilo'],
+    '5 kilo': ['4 kilo', 'Bole'],
+    'Bole': ['5 kilo', 'Megenagna', 'Kazanchis'],
+    'Megenagna': ['Piasa', 'Bole', 'Mexico', 'Koye'],
+    'Merkato': ['Piasa', 'Autobistera'],
+    'Kazanchis': ['Bole'],
+    'Autobistera': ['Merkato'],
+    'Mexico': ['Bole', 'Megenagna', '4 kilo'],
+    'Torhayloch': ['Mexico'],
+    'Abinet': ['Mexico', 'Merkato', 'Piasa'],
+    'Koye': ['Megenagna'],
+    'Mesalemya': ['Merkato']
 }
-
 
 
 # Implementing Breadth-First Search (BFS)
@@ -41,7 +39,7 @@ def bfs(graph, start, goal):
         if current_node == goal:
             print("BFS nodes expanded:", node_count)
             return current_path
-        
+
         for neighbor_node in graph.get(current_node, []):
             new_path = list(current_path)
             new_path.append(neighbor_node)
@@ -52,9 +50,8 @@ def bfs(graph, start, goal):
     
 
 
-
 # Implementing Depth-First Search
-def dfs(graph, start, goal, explored_cities=None, node_count = None):
+def dfs(graph, start, goal, explored_cities=None, node_count=None):
     if explored_cities is None:
         explored_cities = set()
         node_count = [0]
@@ -62,18 +59,16 @@ def dfs(graph, start, goal, explored_cities=None, node_count = None):
     explored_cities.add(start)
     node_count[0] += 1
     if start == goal:
-        print ("DFS nodes expanded:", node_count[0])
+        print("DFS nodes expanded:", node_count[0])
         return [start]
     
-
     for neighbor_node in graph.get(start, []):
         if neighbor_node not in explored_cities:
             current_path = dfs(graph, neighbor_node, goal, explored_cities, node_count)
             if current_path:
                 return [start] + current_path
-            
-    return None
 
+    return None
 
 
 # Implementing Uniform-Cost Search
@@ -90,6 +85,7 @@ def ucs(graph, start, goal):
             continue
 
         explored_cities.add(current_node)
+        node_count += 1
         if current_node == goal:
             print("UCS nodes expanded:", node_count)
             return current_path
@@ -127,6 +123,7 @@ def iddfs(graph, start, goal):
     node_count = [0]
     for depth in range(len(graph)):
         explored_cities = set()
+        node_count[0] = 0
         current_path = depth_limited_search(start, goal, depth, explored_cities, node_count)  
         if current_path:
             print("IDDFS nodes expanded:", node_count[0])
@@ -136,21 +133,30 @@ def iddfs(graph, start, goal):
     return None
 
 
-
-
 # Travel selection:
-
 print("Welcome to Addis.")
 print("From where are you starting your journey? ")
 print(", ".join(graph.keys()))
 
-start = input("\nYour strating place is: ")
-print("Where is you destination? ")
+start = input("\nYour starting place is: ")
+print("Where is your destination? ")
 print(", ".join(graph.keys()))
 goal = input("Your destination is: ")
 
+if start not in graph or goal not in graph:
+    print("Error: One or both locations are not valid!")
+    exit()
 
-print("\nBFS Path:", bfs(graph, start, goal))
-print("DFS Path:", dfs(graph, start, goal))
-print("UCS Path:", ucs(graph, start, goal))
-print("IDDFS Path:", iddfs(graph, start, goal))
+
+
+bfs_path = bfs(graph, start, goal)
+dfs_path = dfs(graph, start, goal)
+ucs_path = ucs(graph, start, goal)
+iddfs_path = iddfs(graph, start, goal)
+
+# Display results
+print("\nResults:")
+print("BFS Path:", bfs_path if bfs_path else "No path found")
+print("DFS Path:", dfs_path if dfs_path else "No path found")
+print("UCS Path:", ucs_path if ucs_path else "No path found")
+print("IDDFS Path:", iddfs_path if iddfs_path else "No path found")
